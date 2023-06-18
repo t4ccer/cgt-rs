88194e1bcb109274747e04015513a4144577c429
t4ccer <t4ccer@users.noreply.github.com>
2023-06-18 15:03:43 +0000

    deploy: b8d16c9831e773000667057bce863c8146dc8c56

--- cgt-website-b4098c00adbabf03.js
+++ cgt-website-b4098c00adbabf03.js
@@ -495,8 +495,8 @@ imports.wbg.__wbindgen_closure_wrapper156 = function(arg0, arg1, arg2) {
     const ret = makeMutClosure(arg0, arg1, 29, __wbg_adapter_16);
     return addHeapObject(ret);
 };
-imports.wbg.__wbindgen_closure_wrapper430 = function(arg0, arg1, arg2) {
-    const ret = makeMutClosure(arg0, arg1, 168, __wbg_adapter_19);
+imports.wbg.__wbindgen_closure_wrapper580 = function(arg0, arg1, arg2) {
+    const ret = makeMutClosure(arg0, arg1, 209, __wbg_adapter_19);
     return addHeapObject(ret);
 };
 
@@ -537,7 +537,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('cgt-website-652a6954f2ff0bfa_bg.wasm', import.meta.url);
+        input = new URL('cgt-website-b4098c00adbabf03_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
