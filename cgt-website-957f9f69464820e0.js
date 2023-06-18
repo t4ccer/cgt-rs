95ee393b03cf32410e78b4cf951e0af331202e32
t4ccer <t4ccer@users.noreply.github.com>
2023-06-18 16:56:08 +0000

    deploy: 8b16380cadbf3eb0dd50b47810750c54e264a19c

--- cgt-website-957f9f69464820e0.js
+++ cgt-website-957f9f69464820e0.js
@@ -496,7 +496,7 @@ imports.wbg.__wbindgen_closure_wrapper156 = function(arg0, arg1, arg2) {
     return addHeapObject(ret);
 };
 imports.wbg.__wbindgen_closure_wrapper580 = function(arg0, arg1, arg2) {
-    const ret = makeMutClosure(arg0, arg1, 209, __wbg_adapter_19);
+    const ret = makeMutClosure(arg0, arg1, 210, __wbg_adapter_19);
     return addHeapObject(ret);
 };
 
@@ -537,7 +537,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('cgt-website-b4098c00adbabf03_bg.wasm', import.meta.url);
+        input = new URL('cgt-website-957f9f69464820e0_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
