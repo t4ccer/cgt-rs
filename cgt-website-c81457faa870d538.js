8f2fbbbc9bd6be9a32aac176a8f33d4dafda7128
t4ccer <t4ccer@users.noreply.github.com>
2023-07-09 12:41:54 +0000

    deploy: 6d46dfc42c6ecc087c964d94e1f38f784156980e

--- cgt-website-c81457faa870d538.js
+++ cgt-website-c81457faa870d538.js
@@ -518,7 +518,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('cgt-website-58acf9ce505970c6_bg.wasm', import.meta.url);
+        input = new URL('cgt-website-c81457faa870d538_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
